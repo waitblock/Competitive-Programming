4d84ff4a945c774b3a2b8f6d843d210d779c7d1f
Ethan <65051086+waitblock@users.noreply.github.com>
2021-07-07 14:27:59 -0400

    Create template.java

---
+++ templates/template.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class template {
+  public static void main(String[] args) throws FileNotFoundException {
+    Scanner in = new Scanner(new File("problem.in"));
+    ArrayList<String> lines = new ArrayList<String>();
+    while(in.hasNextLine()){
+      lines.add(in.nextLine());
+    }
+    in.close();
+
+    int result = 0;
+    PrintWriter out = new PrintWriter(new File("problem.out"));
+    out.println(result);
+    out.close();
+  }
+}
