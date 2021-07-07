12a6bcbfaf9329056ec0dccf2f2cb7b3fc6c5d3d
Ethan <65051086+waitblock@users.noreply.github.com>
2021-07-07 14:35:48 -0400

    python template

---
+++ USACO/templates/template.py
@@ -0,0 +1,18 @@
+import string
+import math
+import itertools
+
+
+def convert_arr_int(arr):
+    return list(map(int, arr))
+
+
+def main():
+    with open("problem.in", "r") as input_file:
+        input_data = input_file.read()
+    # TODO: SOLVE
+    output_file = open("problem.out", "w")
+
+
+if __name__ == "__main__":
+    main()
