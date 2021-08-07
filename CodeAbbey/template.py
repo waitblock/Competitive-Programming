c10cfd99c9bfd4a04e43f2b1d1fff8f8a6fdb52b
waitblock <65051086+waitblock@users.noreply.github.com>
2021-08-07 14:43:04 -0400

    update template to include modified rounding

--- CodeAbbey/template.py
+++ CodeAbbey/template.py
@@ -1,3 +1,10 @@
+import math
+
+def round_int(n):
+    if n >= math.floor(n) + 0.5:
+        return math.ceil(n)
+    return math.floor(n)
+
 def convert_string_int_arr(iterable):
     return list(map(int, iterable))
 
