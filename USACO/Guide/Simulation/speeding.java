729282272d6e0da9a6efcaf445fcefe1b5c6b835
Ethan <65051086+waitblock@users.noreply.github.com>
2022-01-22 22:58:46 -0500

    :cow:

---
+++ USACO/Guide/Simulation/speeding.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class speeding {
+  public static void main(String[] args) throws FileNotFoundException {
+    Scanner in = new Scanner(new File("speeding.in"));
+    int n = in.nextInt();
+    int m = in.nextInt();
+
+    int[] limits = new int[100];
+    int[] speeds = new int[100];
+
+    int ind = 0;
+
+    for(int i = 0; i<n; i++){
+      int length = in.nextInt();
+      int limit = in.nextInt();
+      for(int j = ind; j<ind+length; j++){
+        limits[j] = limit;
+      }
+      ind += length;
+    }
+
+    ind = 0;
+
+    for(int i = 0; i<n; i++){
+      int length = in.nextInt();
+      int speed = in.nextInt();
+      for(int j = ind; j<ind+length; j++){
+        speeds[j] = speed;
+      }
+      ind += length;
+    }
+
+    int result = 0;
+    for(int i = 0; i<100; i++){
+      result = Math.max(speeds[i]-limits[i], result);
+    }
+    PrintWriter out = new PrintWriter(new File("speeding.out"));
+    out.println(result);
+    System.out.println(result);
+    out.close();
+  }
+}
