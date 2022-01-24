369ffddabe700586cac20418f0280f3533238464
Ethan <65051086+waitblock@users.noreply.github.com>
2022-01-24 15:34:24 -0500

    because eric wanted it like this

--- USACO/templates/template.py
+++ USACO/templates/template.py
@@ -11,10 +11,7 @@ def convert_arr_int(arr):
 
 
 def main():
-    with open("problem.in", "r") as input_file:
-        input_data = input_file.read()
-    # TODO: SOLVE
-    output_file = open("problem.out", "w")
+    
 
 
 if __name__ == "__main__":
