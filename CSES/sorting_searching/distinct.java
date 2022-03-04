6bbed6ff54d7fa19dcfce6b33ed4c39463d714ff
Ethan <65051086+waitblock@users.noreply.github.com>
2022-03-03 22:42:33 -0500

    Create distinct.java

---
+++ CSES/sorting_searching/distinct.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ 
+public class distinct {
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    HashSet<Integer> hs = new HashSet<>();
+    for(int i = 0; i<n; i++){
+      hs.add(in.nextInt());
+    }
+    System.out.println(hs.size());
+  }
+}
