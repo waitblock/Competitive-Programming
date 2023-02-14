ffecc602a7ef32bd6bb45e9d9124f5a8d0171a72
Ethan <65051086+waitblock@users.noreply.github.com>
2023-02-13 22:11:17 -0500

    asdf

---
+++ USACO/Momentum/Silver_A/Homework04/homework.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public final class homework {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new File("homework.in"));
+        
+        in.close();
+    }
+}
--- USACO/Momentum/Silver_A/Homework04/truckload.java
+++ USACO/Momentum/Silver_A/Homework04/truckload.java
@@ -1,45 +1,61 @@
 import java.util.*;
+import java.io.*;
 
 public final class truckload {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int m = in.nextInt();
-        int[] units = new int[n];
-        int[] price = new int[n];
-        int[] capacity = new int[m];
-        for(int i = 0; i<n; i++){
-            units[i] = in.nextInt();
-            price[i] = in.nextInt();
-        }
-        for(int i = 0; i<m; i++){
-            capacity[i] = in.nextInt();
-        }
-        in.close();
-        int[] load = new int[m];
-        int[] truckPrice = new int[m];
-        int unitsInd = 0;
-        int loadInd = 0;
-        while(true){
-            if(unitsInd >= n || loadInd >= m) break;
-            int req = capacity[loadInd]-load[loadInd];
-            int remaining = Math.max(0,units[unitsInd]-req);
-            int fulfilled = units[unitsInd]-remaining;
-            load[loadInd] += fulfilled;
-            truckPrice[loadInd] += (fulfilled * price[unitsInd]);
-            units[unitsInd] = remaining;
-            if(capacity[loadInd] == load[loadInd]){
-                loadInd++;
-            }
-            if(units[unitsInd] == 0){
-                unitsInd++;
-            }
-        }
-        // System.out.println(Arrays.toString(truckPrice));
-        int result = 0;
-        for(int i = 0; i<m; i++){
-            result += truckPrice[i];
-        }
-        System.out.println(result);
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter directory: ");
+    String dir = in.next();
+    while(true){
+      System.out.print("Enter filename: ");
+      String f = in.next();
+      try{
+        solve(dir+"/"+f);
+      }
+      catch(Exception e){
+        e.printStackTrace(System.err);
+      }
     }
+  }
+  static void solve(String filename) throws FileNotFoundException {
+    Scanner in = new Scanner(new File(filename));
+    int n = in.nextInt();
+    int m = in.nextInt();
+    int[] units = new int[n];
+    int[] price = new int[n];
+    int[] capacity = new int[m];
+    for(int i = 0; i<n; i++){
+        units[i] = in.nextInt();
+        price[i] = in.nextInt();
+    }
+    for(int i = 0; i<m; i++){
+        capacity[i] = in.nextInt();
+    }
+    in.close();
+    int[] load = new int[m];
+    int[] truckPrice = new int[m];
+    int unitsInd = 0;
+    int loadInd = 0;
+    while(true){
+        if(unitsInd >= n || loadInd >= m) break;
+        int req = capacity[loadInd]-load[loadInd];
+        int remaining = Math.max(0,units[unitsInd]-req);
+        int fulfilled = units[unitsInd]-remaining;
+        load[loadInd] += fulfilled;
+        truckPrice[loadInd] += (fulfilled * price[unitsInd]);
+        units[unitsInd] = remaining;
+        if(capacity[loadInd] == load[loadInd]){
+            loadInd++;
+        }
+        if(units[unitsInd] == 0){
+            unitsInd++;
+        }
+    }
+    // System.out.println(Arrays.toString(truckPrice));
+    int result = 0;
+    for(int i = 0; i<m; i++){
+        result += truckPrice[i];
+    }
+    System.out.println(result);
+  }
 }
