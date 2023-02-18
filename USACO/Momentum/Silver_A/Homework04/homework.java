dc33647400522caf52ef4a09334f6ea9a9290cfe
Ethan <65051086+waitblock@users.noreply.github.com>
2023-02-17 21:54:06 -0500

    a

--- USACO/Momentum/Silver_A/Homework04/homework.java
+++ USACO/Momentum/Silver_A/Homework04/homework.java
@@ -4,7 +4,36 @@ import java.io.*;
 public final class homework {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(new File("homework.in"));
-        
+        int n = in.nextInt();
+        int[] points = new int[n];
+        for(int i = 0; i<n; i++){
+            points[i] = in.nextInt();
+        }
         in.close();
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+		HashSet<Integer> maxInd = new HashSet<>();
+        double max = Integer.MIN_VALUE;
+        for(int i = n-1; i>=1; i--){
+            sum += points[i];
+            min = Math.min(min, points[i]);
+            if(i > n-2) continue;
+            double temp = (sum-min)/(n-i-1);
+            if(temp > max){
+                max = temp;
+                maxInd.clear();
+                maxInd.add(i);
+            }
+            if(temp == max){
+                maxInd.add(i);
+            }
+
+		}
+        PrintWriter out = new PrintWriter(new File("homework.out"));
+        for(int i : maxInd){
+            out.println(i);
+            System.out.println(i);
+        }
+        out.close();
     }
 }
