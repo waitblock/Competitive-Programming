d1e6084810f0b82c631e5caa2bc116b38115a6de
Ethan <65051086+waitblock@users.noreply.github.com>
2023-02-21 16:34:30 -0500

    Create art.java

---
+++ USACO/Momentum/Silver_A/Homework05/art.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+public final class art {
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner in = new Scanner(new File("art.in"));
+		
+	}
+}
