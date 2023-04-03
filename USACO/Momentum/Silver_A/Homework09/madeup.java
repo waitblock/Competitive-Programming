621a5222fa5ab462aeefb09b3a8c51ea5620d493
ethan4748 <81245565+ethan4748@users.noreply.github.com>
2023-04-03 11:22:40 -0400

    Update madeup.java

--- USACO/Momentum/Silver_A/Homework09/madeup.java
+++ USACO/Momentum/Silver_A/Homework09/madeup.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public final class Main {
+public final class madeup {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
